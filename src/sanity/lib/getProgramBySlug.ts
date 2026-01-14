0012e8a12c56eda60cdf51c3c889facc74c063a5
gp31baseball <james.snell@darkvisiontech.com>
2026-01-14 09:56:08 -0600

    Fix GROQ slug query parsing

--- src/sanity/lib/getProgramBySlug.ts
+++ src/sanity/lib/getProgramBySlug.ts
@@ -2,15 +2,15 @@ import { client } from '@/sanity/lib/client';
 
 export async function getProgramBySlug(slug: string) {
   return client.fetch(
-    
-    *[_type == "program" && slug.current == \][0]{
+    `
+    *[_type == "program" && slug.current == $slug][0]{
       name,
       summary,
       body,
       image,
       gallery
     }
-    ,
+    `,
     { slug }
   );
 }
